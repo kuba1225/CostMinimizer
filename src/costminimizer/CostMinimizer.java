52efb0e93860ef54c53184c5651829305dd267e8
Jakub Barański <kubabar1@interia.pl>
2017-12-04 18:24:22 +0100

    Dodanie wstępnej implementacji głównej części programu

--- src/costminimizer/CostMinimizer.java
+++ src/costminimizer/CostMinimizer.java
@@ -1,9 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package costminimizer;
+
+import algorithms.BellmanFordAlgorithm;
+import algorithms.EdmondsKarpAlgorithm;
+import dynamicstructures.*;
+import iocommunication.Parser;
+import iocommunication.Writer;
+import java.util.ArrayList;
 
 /**
  *
@@ -11,11 +13,42 @@ package costminimizer;
  */
 public class CostMinimizer {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static RedBlackTree<Integer, Farm> farm;
+    public static RedBlackTree<Integer, Shop> shop;
+    public static Matrix matrixConstraints;
+    public static Matrix matrixCosts;
+    public static Matrix matrixShopsOrders;
+    public static Matrix matrixFermsNumberEggs;
+    public static ArrayList<Graph> residualGraph;
+
+    public CostMinimizer() {
+        farm = new RedBlackTree<>();
+        shop = new RedBlackTree<>();
+        residualGraph = new ArrayList<>();
     }
-    
+
+    public void minimizeCost() {
+        CostMinimizer c = new CostMinimizer();
+        Parser r = new Parser("test.txt");
+        r.readFile();
+        EdmondsKarpAlgorithm e = new EdmondsKarpAlgorithm();
+        System.out.println("Przetwarzanie grafu algorytmem Edmondsa-Karpa...");
+        e.edmondsKarp();
+        BellmanFordAlgorithm bf = new BellmanFordAlgorithm();
+        System.out.println("Przetwarzanie grafu algorytmem Bellmana-Forda...");
+        //bf.bellmanFordAlgorithm();
+        System.out.println("Suma całkowita kosztów przed usuwaniem ujemnych cykli to: " + bf.obliczSume() + " $");
+        while (!bf.bellmanFordAlgorithm()) {
+            bf.usunUjemnyCykl();
+        }
+        Writer w = new Writer();
+        w.wypiszOptymalneRozwiazanie();
+        System.out.println("Suma całkowita kosztów po usunięciu ujemych cykli to: " + bf.obliczSume() + " $");
+    }
+
+    public static void main(String[] argv) {
+        CostMinimizer c = new CostMinimizer();
+        c.minimizeCost();
+    }
+
 }
