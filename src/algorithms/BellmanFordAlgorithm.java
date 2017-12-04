946927cf3baf957d4dbaf9c90fb541282534c1c9
Jakub Barański <kubabar1@interia.pl>
2017-12-04 18:20:58 +0100

    Dodanie implementacji algorytmów

--- src/algorithms/BellmanFordAlgorithm.java
+++ src/algorithms/BellmanFordAlgorithm.java
@@ -1,14 +1,297 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package algorithms;
 
-/**
- *
- * @author Kuba
- */
+import static costminimizer.CostMinimizer.*;
+import dynamicstructures.*;
+import static java.lang.Math.abs;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class BellmanFordAlgorithm {
-    
+
+    public static final int MAXINT = Integer.MAX_VALUE;
+    ArrayList<Integer> negativeCycle = new ArrayList<>();
+
+    private int edges, verticles;                           //liczba krawędzi i wierzchołków grafu
+    private verticleElement[] A;                            //tablica list sąsiedztwa
+    private int[] previousElementArray;                     //tablica poprzedników
+    private long[] costArray;                               //tablicza kosztów dojścia do wierzchołków
+
+    private class verticleElement {
+
+        verticleElement next;
+        int nextVerticle;       //numer następnego węzła
+        int cost;               //koszt przewozu
+    }
+
+    public boolean bellmanFordAlgorithm() {
+        int x, y, w;               //zmienne pomocnicze
+        verticleElement pv;
+
+        verticles = matrixConstraints.getColumnNumber() + matrixConstraints.getRowNumber() + 2;
+        edges = residualGraph.size();
+
+        A = new verticleElement[verticles];             //tablica list sąsiedztwa
+        costArray = new long[verticles];                //tablicza kosztów dojścia do wierzchołków
+        previousElementArray = new int[verticles];      //tablica poprzedników
+
+        for (int i = 0; i < verticles; i++) {
+            costArray[i] = MAXINT;
+            previousElementArray[i] = -1;//-1 oznacza, że element nie został jeszcze odwiedzony
+            A[i] = null;
+        }
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~WCZYTYWANIE DANYCH~~~~~~~~~~~~~~~~~~~~~~~~~~
+        for (int i = 0; i < edges; i++) {
+            x = residualGraph.get(i).getFrom();
+            y = residualGraph.get(i).getTo();
+            w = residualGraph.get(i).getCost();
+
+            pv = new verticleElement();
+            pv.nextVerticle = y;
+            pv.cost = w;
+            pv.next = A[x];
+            A[x] = pv;
+        }
+
+        return bellmanFord(0);
+    }
+
+    public boolean bellmanFord(int startVerticle) {//startVerticle - wierzchołek początkowy
+        int i, x;
+        boolean noChanges;
+        verticleElement pv;
+
+        costArray[startVerticle] = 0;//wyzerowanie kosztu dojścia do wierzchołka początkowego
+        for (i = 1; i < verticles; i++) {
+            noChanges = true;//brak zmian w tablicach kosztów i poprzedników
+            for (x = 0; x < verticles; x++) {
+                for (pv = A[x]; pv != null; pv = pv.next)//przechodzimy po wszystkich sąsiadach listy wierzchołka x
+                {
+                    if (costArray[pv.nextVerticle] > costArray[x] + pv.cost)//warunek relaksacji
+                    {
+                        noChanges = false;//jest zmiana w tablicach kosztów i poprzedników
+                        costArray[pv.nextVerticle] = costArray[x] + pv.cost; //relaksacja krawędzi z x do sąsiada
+                        previousElementArray[pv.nextVerticle] = x;           //ustawiamy wierzchołek x jako poprzednik sąsiada
+                    }
+                }
+            }
+            if (noChanges) {
+                return true;         //brak zmian -> koniec
+            }
+        }
+
+        // Sprawdzamy istnienie ujemnego cyklu
+        for (x = 0; x < verticles; x++) {
+            for (pv = A[x]; pv != null; pv = pv.next) {
+                if (costArray[pv.nextVerticle] > costArray[x] + pv.cost) {//sprawdzamy czy istnieją krawędzie nie spełniające warunku relaksacji
+                    //wypisz_ujemny_cykl(A, pv, d);
+                    findNegativeCycle(pv.nextVerticle);
+                    return false; //false oznacza znalezienie ujemnego cyklu
+                }
+            }
+        }
+        return true;
+    }
+
+    public void findNegativeCycle(int z) {
+        negativeCycle.clear();
+
+        char[] colors = new char[verticles];
+        for (int i = 0; i < verticles; i++) {
+            colors[i] = 'w';//kolorujemy wszystkie węzły na biały kolor
+        }
+        int start = z;
+        int poprzednik = previousElementArray[start];
+
+        negativeCycle.add(start);
+
+        colors[start] = 'g';//węzeł początkowy kolorujemy na kolor szary
+        while (colors[poprzednik] != 'g' && poprzednik >= 0) {//przeszukiwanie trwa do czasu gdy dojdziemy do węzła szarego
+            colors[poprzednik] = 'g';
+
+            negativeCycle.add(poprzednik);
+            poprzednik = previousElementArray[poprzednik];
+        }
+        negativeCycle.add(poprzednik);
+        Collections.reverse(negativeCycle);
+
+        int st = negativeCycle.get(0);
+
+        int rozmiar;
+
+        for (rozmiar = 1; rozmiar < negativeCycle.size(); rozmiar++) {
+            if (negativeCycle.get(rozmiar) == st) {
+                break;//upewniamy się że cykl ujemny jest zamknięty
+            }
+        }
+
+        rozmiar++;
+
+        for (; rozmiar < negativeCycle.size(); rozmiar++) {
+            negativeCycle.remove(rozmiar);//usuwamy z cyklu wszystkie krawędzie które są poza jego ,,zamknięciem''
+        }
+    }
+
+    public void usunUjemnyCykl() {
+        int x, y, f, c;
+        int minFlow;
+        int[] cycleFlowTab = new int[negativeCycle.size() - 1];
+        int t = 0;
+
+        //1.przeszukiwanie grafu w obrebie cyklu ujemnego w celu znalezienia najmniejszego przepływu
+        for (int i = 0; i < negativeCycle.size() - 1; i++) {
+            for (int j = 0; j < edges; j++) {
+                x = residualGraph.get(j).getFrom();
+                y = residualGraph.get(j).getTo();
+                if (x == negativeCycle.get(i) && y == negativeCycle.get(i + 1)) {
+                    cycleFlowTab[t] = residualGraph.get(j).getFlow();
+                    //System.out.println(residualGraph.get(j).getFlow());
+                    t++;
+                }
+            }
+        }
+
+        minFlow = findMin(cycleFlowTab);
+
+        //2.w zaleznosci w ktora strone cyklu przechodzi się, należy dodać lub odejąć znalezioną najnizszą wartość przepływu
+        ArrayList<Graph> tmpResidualGraph = new ArrayList<>();
+        int flowConstraint;
+        boolean wstawionoUjemyCykl;
+        boolean test = true;
+
+        if (minFlow > 0) {
+            for (int j = 0; j < residualGraph.size(); j++) {
+                x = residualGraph.get(j).getFrom();
+                y = residualGraph.get(j).getTo();
+                f = residualGraph.get(j).getFlow();
+                c = residualGraph.get(j).getCost();
+                wstawionoUjemyCykl = false;
+
+                for (int i = 0; i < negativeCycle.size() - 1; i++) {
+                    if (negativeCycle.get(i) == x && negativeCycle.get(i + 1) == y) {
+                        wstawionoUjemyCykl = true;
+                        if (y > x) {//przechodzimy w grafie w prawo (dodatnie części cyklu)
+                            if (x == 0) {
+                                flowConstraint = (int) matrixFermsNumberEggs.get(y - 1, 0);
+                            } else if (y == (matrixConstraints.getColumnNumber() + matrixConstraints.getRowNumber() + 1)) {
+                                flowConstraint = (int) matrixShopsOrders.get(0, x - matrixConstraints.getRowNumber() - 1);
+                            } else {
+                                flowConstraint = (int) matrixConstraints.get(x - 1, y - matrixConstraints.getRowNumber() - 1);
+                            }
+
+                            if ((f + minFlow) == flowConstraint) {//przypadek 2
+                                tmpResidualGraph.add(new Graph(y, x, flowConstraint, -(abs(c))));
+                            } else {//przypadek 3
+                                if ((flowConstraint - (flowConstraint - f + minFlow)) > 0) {
+                                    tmpResidualGraph.add(new Graph(x, y, flowConstraint - (flowConstraint - f + minFlow), abs(c)));
+                                }
+                                if ((flowConstraint - f + minFlow) > 0) {
+                                    tmpResidualGraph.add(new Graph(y, x, (flowConstraint - f + minFlow), -(abs(c))));
+                                }
+                            }
+                            if (f != 0 && f != flowConstraint && test) {
+                                j++;
+                            }
+                            test = true;
+                        } else if (x > y) {//przechodzimy w grafie w lewo (ujemne części cyklu)
+                            if (y == 0) {
+                                flowConstraint = (int) matrixFermsNumberEggs.get(x - 1, 0);
+                            } else if (x == (matrixConstraints.getColumnNumber() + matrixConstraints.getRowNumber() + 1)) {
+                                flowConstraint = (int) matrixShopsOrders.get(0, y - matrixConstraints.getRowNumber() - 1);
+                            } else {
+                                flowConstraint = (int) matrixConstraints.get(y - 1, x - matrixConstraints.getRowNumber() - 1);
+                            }
+
+                            if ((f - minFlow) == 0) {//przypadek 1
+                                tmpResidualGraph.add(new Graph(y, x, flowConstraint, (abs(c))));
+                            } else {//przypadek 3
+                                if ((f - minFlow) > 0) {
+                                    tmpResidualGraph.add(new Graph(x, y, (f - minFlow), -(abs(c))));
+                                }
+                                if ((flowConstraint - (f - minFlow)) > 0) {
+                                    tmpResidualGraph.add(new Graph(y, x, flowConstraint - (f - minFlow), abs(c)));
+                                }
+                            }
+                            if (f != 0 && f != flowConstraint && test) {
+                                j++;
+                            }
+                            test = true;
+                        }
+                    } else if (negativeCycle.get(i) == y && negativeCycle.get(i + 1) == x) {
+                        wstawionoUjemyCykl = true;
+                        test = false;
+                    }
+                }
+                if (!wstawionoUjemyCykl) {
+                    tmpResidualGraph.add(residualGraph.get(j));
+                }
+
+            }
+
+            //3.zamiana grafu oryginalnego z tymczasowym
+            residualGraph.clear();
+            residualGraph = tmpResidualGraph;
+
+            //System.out.println("Suma całkowita kosztów to: " + obliczSume() + " $");
+        }
+    }
+
+    public int findMin(int[] x) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < x.length; i++) {
+            if (x[i] < min && x[i] > 0) {
+                min = x[i];
+            }
+        }
+        //System.out.println("MinFlow = " + minFlow);
+        return min;
+    }
+
+    public void writeResidualGraph() {
+        for (Graph g : residualGraph) {
+            System.out.println(g.getFrom() + " -> " + g.getTo() + "  przepływ:" + g.getFlow() + "  koszt:" + g.getCost());
+        }
+    }
+
+    public void writeNegativeCycle() {
+        int j = 0;
+        System.out.println("~~~~~~NegativeCycle~~~~~~~~");
+        for (int i : negativeCycle) {
+            j++;
+            System.out.print(i);
+            if (j < negativeCycle.size()) {
+                System.out.print(" -> ");
+            }
+        }
+        System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+
+    public int getNumberOfEdgeInResidualGraph(int from, int to) {
+        int x, y;
+        for (int j = 0; j < residualGraph.size(); j++) {
+            x = residualGraph.get(j).getFrom();
+            y = residualGraph.get(j).getTo();
+
+            if (x == from && y == to) {
+                return j;//numer krawędzi w liście reprezentującej graf rezydualny
+            }
+        }
+        return -1;//w grafie nie istnieje krawęd łącząca dwa punkty
+    }
+
+    public int obliczSume() {
+        int liczbaFerm = matrixConstraints.getRowNumber();
+        int licbaSklepow = matrixConstraints.getColumnNumber();
+        int suma = 0;
+
+        for (int i = liczbaFerm + 1; i <= licbaSklepow + liczbaFerm; i++) {
+            for (int j = 1; j <= liczbaFerm; j++) {
+                if (getNumberOfEdgeInResidualGraph(i, j) != -1) {
+                    suma += (abs(residualGraph.get(getNumberOfEdgeInResidualGraph(i, j)).getCost()) * abs(residualGraph.get(getNumberOfEdgeInResidualGraph(i, j)).getFlow()));
+                }
+            }
+        }
+        return suma;
+    }
+
 }
--- src/algorithms/EdmondsKarpAlgorithm.java
+++ src/algorithms/EdmondsKarpAlgorithm.java
@@ -1,14 +1,226 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package algorithms;
+
+import static costminimizer.CostMinimizer.*;
+import dynamicstructures.*;
 
 /**
  *
  * @author Kuba
  */
 public class EdmondsKarpAlgorithm {
-    
+
+    public static final int MAXINT = Integer.MAX_VALUE;
+
+    private class verticleElement {
+
+        verticleElement next;
+        int nextVerticle;//numer następnego węzła
+        int constraints;//ograniczenia przepływu
+        int flow;//przepływ
+    };
+
+    public void edmondsKarp() {
+        int edges, verticles;                   //liczba krawędzi i wierzchołków
+        verticleElement[] neighboursArray;      //tablica list sąsiedztwa
+        int startVerticle, finalVerticle;                               //wierzchołki źródła i ujścia
+        Queue queue = new Queue();
+        int[] previousElementArray;             //tablica poprzedników
+        int[] capacityResidualFlowArray;        //tablica przepustowości rezydualnych
+        int cp, u, v;
+        verticleElement x, z;                   //wskaźniki elementów listy sąsiedztwa
+        boolean testEdge = false;
+
+        //pobranie liczby wierzchołków i krawędzi
+        verticles = matrixConstraints.getColumnNumber() + matrixConstraints.getRowNumber() + 2;
+        edges = matrixConstraints.getColumnNumber() * matrixConstraints.getRowNumber() + (matrixConstraints.getColumnNumber() + matrixConstraints.getRowNumber());
+
+        startVerticle = 0;              //wierzchołek źródła
+        finalVerticle = verticles - 1;  //wierzchołek ujścia
+
+        neighboursArray = new verticleElement[verticles];
+        for (int i = 0; i < verticles; i++) {
+            neighboursArray[i] = null;          //tablicę sąsiedztwa na początku należy wypełnić pustymi listami
+        }
+        previousElementArray = new int[verticles];
+        capacityResidualFlowArray = new int[verticles];
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~WCZYTYWANIE DANYCH~~~~~~~~~~~~~~~~~~~~~~~~~~
+        for (int it = 0; it < matrixConstraints.getRowNumber(); it++) {
+            for (int jt = 0; jt < matrixConstraints.getColumnNumber(); jt++) {
+                x = new verticleElement();
+                x.nextVerticle = jt + matrixConstraints.getRowNumber() + 1;
+                x.constraints = (int) matrixConstraints.get(it, jt);
+                x.flow = 0;
+                x.next = neighboursArray[it + 1];
+                neighboursArray[it + 1] = x;
+            }
+        }
+
+        for (int it = 0; it < matrixConstraints.getRowNumber(); it++) {
+            x = new verticleElement();
+            x.nextVerticle = it + 1;
+            x.constraints = (int) matrixFermsNumberEggs.get(it, 0);
+            x.flow = 0;
+            x.next = neighboursArray[0];
+            neighboursArray[0] = x;
+
+        }
+
+        for (int it = 0; it < matrixConstraints.getColumnNumber(); it++) {
+            x = new verticleElement();
+            x.nextVerticle = matrixConstraints.getColumnNumber() + matrixConstraints.getRowNumber() + 1;
+            x.constraints = (int) matrixShopsOrders.get(0, it);
+            x.flow = 0;
+            x.next = neighboursArray[it + matrixConstraints.getRowNumber() + 1];
+            neighboursArray[it + matrixConstraints.getRowNumber() + 1] = x;
+
+        }
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        //~~~~~~~~~~~~~~~~~~~~~Tworzenie grafu rezydualnego~~~~~~~~~~~~~~~~~~~~~
+        for (int i = 0; i < verticles; i++) {
+            for (x = neighboursArray[i]; x != null; x = x.next) {
+
+                testEdge = false;
+                for (z = neighboursArray[x.nextVerticle]; z != null; z = z.next) {
+                    if (z.nextVerticle == i) {
+                        testEdge = true;
+                        break;
+                    }
+                }
+                if (testEdge) {
+                    continue;
+                }
+
+                z = new verticleElement();
+                z.nextVerticle = i;
+                z.constraints = z.flow = 0;
+                z.next = neighboursArray[x.nextVerticle];
+                neighboursArray[x.nextVerticle] = z;
+            }
+        }
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        while (true) {
+            for (int i = 0; i < verticles; i++) {
+                previousElementArray[i] = -1;   //wyzerowanie tablicy poprzedników (-1 oznacza, że ten element nie został jeszcze odwiedzony)
+            }
+            capacityResidualFlowArray[startVerticle] = MAXINT;  //ustawienie nieskończonej przepustowości źródła
+
+            while (!queue.empty()) {
+                queue.pop();    //wyzerowanie kolejki
+            }
+            queue.push(startVerticle);//umieszczenie źródła w kolejce
+
+            /*poszukiwanie ścieżki w sieci rezydualnej od s do t*/
+            while (!queue.empty()) {
+                testEdge = false;
+                u = queue.pop();   //pobranie wierzchołka
+
+                for (x = neighboursArray[u]; x != null; x = x.next) {//przegląznie listy sąsiadów wierzchołka u
+                    cp = x.constraints - x.flow;//przepustowość rezydualna
+
+                    if (cp != 0 && (previousElementArray[x.nextVerticle] == -1)) {
+                        previousElementArray[x.nextVerticle] = u;
+
+                        capacityResidualFlowArray[x.nextVerticle] = cp < capacityResidualFlowArray[u] ? cp : capacityResidualFlowArray[u];//przepustowość rezydualna krawędzi x->nextVerticle
+
+                        if (x.nextVerticle == finalVerticle) {//czy sciezka siega do ujscia
+                            testEdge = true;      //znaleziono sciezke
+                            break;
+                        } else {
+                            queue.push(x.nextVerticle);      // Inaczej umieszczamy w kolejce wierzchołek x->v
+                        }
+                    }
+                }
+
+                if (testEdge) {
+                    break;
+                }           // znaleziono ścieżkę -> wychodzimy z pętli while
+            }
+
+            if (!testEdge) {
+                break;//brak ścieżki -> koniec algorytmu
+            }
+
+            // Przechodzimy wstecz grafu (od finalVerticle do startVerticle)
+            for (v = finalVerticle; v != startVerticle; v = u) {
+                u = previousElementArray[v];                   // u to poprzednik v
+
+                // Szukamy na liście sąsiadów u krawędzi prowadzącej do v.
+                for (z = neighboursArray[u]; z != null; z = z.next) {
+                    if (z.nextVerticle == v) {
+                        z.flow += capacityResidualFlowArray[finalVerticle];         //zwiększenie prezpływu w kierunku zgodnym
+                        break;
+                    }
+                }
+
+                // Szukamy na liście sąsiadów v krawędzi prowadzącej do u.
+                for (z = neighboursArray[v]; z != null; z = z.next) {
+                    if (z.nextVerticle == u) {
+                        z.flow -= capacityResidualFlowArray[finalVerticle];         //zmniejszenie przepływu w kierunku przeciwnm
+                        break;
+                    }
+                }
+            }
+        }
+
+        constructResidualGraph(neighboursArray, verticles);
+
+    }
+
+    public void constructResidualGraph(verticleElement[] neighboursArray, int n) {
+        Graph g;
+        verticleElement x, z;
+        int from, to;
+
+        for (int i = 0; i < n; i++) {
+            for (z = neighboursArray[i]; z != null; z = z.next) {
+                if (z.constraints != 0) {
+                    if (i == 0 || i > matrixCosts.getRowNumber()) {//gdy zaczynamy od pierwszego węzła lub gdy dochodzimy do połączeń z ostatnim węzłem
+                        if (z.flow == 0) {//Przypadek nr 1
+                            g = new Graph(i, z.nextVerticle, z.flow, 0);
+                            residualGraph.add(g);
+                        }
+                        if (z.flow == z.constraints) {//Przypadek nr 2
+                            g = new Graph(z.nextVerticle, i, z.flow, 0);
+                            residualGraph.add(g);
+                        }
+                        if (z.flow < z.constraints && z.flow != 0) {//Przypadek nr 3
+                            g = new Graph(i, z.nextVerticle, z.constraints - z.flow, 0);
+                            residualGraph.add(g);
+
+                            g = new Graph(z.nextVerticle, i, z.flow, 0);
+                            residualGraph.add(g);
+                        }
+                    } else {//gdy zaczynamy od jednego z węzłów który jest fermą
+                        if (z.flow == 0) {//Przypadek nr 1
+                            from = i - 1;
+                            to = z.nextVerticle - matrixCosts.getRowNumber() - 1;
+
+                            g = new Graph(i, z.nextVerticle, z.constraints, (int) matrixCosts.get(from, to));
+                            residualGraph.add(g);
+                        }
+                        if (z.flow == z.constraints) {//Przypadek nr 2
+                            from = i - 1;
+                            to = z.nextVerticle - matrixCosts.getRowNumber() - 1;
+                            g = new Graph(z.nextVerticle, i, z.constraints, -(int) matrixCosts.get(from, to));
+                            residualGraph.add(g);
+                        }
+                        if (z.flow < z.constraints && z.flow != 0) {//Przypadek nr 3
+                            from = i - 1;
+                            to = z.nextVerticle - matrixCosts.getRowNumber() - 1;
+
+                            g = new Graph(z.nextVerticle, i, z.flow, -(int) matrixCosts.get(from, to));
+                            residualGraph.add(g);
+
+                            g = new Graph(i, z.nextVerticle, z.constraints - z.flow, (int) matrixCosts.get(from, to));
+                            residualGraph.add(g);
+                        }
+                    }
+
+                }
+            }//residualGraph
+        }
+    }
 }
